badbc9f0e3f0a2582a1d62e10887a59963197218
minhxxk <tactics4421@gmail.com>
2021-12-21 21:46:30 +0900

    CustomDialog 수정

--- app/src/main/java/com/example/diary/DialogCustom.kt
+++ app/src/main/java/com/example/diary/DialogCustom.kt
@@ -1,6 +1,7 @@
 package com.example.diary
 
 import android.app.Dialog
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,30 +9,40 @@ import android.view.View
 import android.view.WindowManager
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.example.diary.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_dialog_custom.*
 
 class DialogCustom : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dialog_custom)
 
         val date = intent.getSerializableExtra("data_date")
         val content = intent.getSerializableExtra("data_title")
-        tv_date_title.setText("$date")
+        tv_date_title.text = "$date"
         et_content.setText("$content")
 
         btn_ok.setOnClickListener(AcceptButtonListener())
         btn_cancel.setOnClickListener(CancelButtonListener())
     }
 
+    //CustomDialog의 확인 버튼 클릭 시
     inner class AcceptButtonListener: View.OnClickListener{
         override fun onClick(v: View?) {
-            Log.i("minhxxk", "확인 버튼 클릭")
+            val database = MainActivity().database
+            var query = "UPDATE DIARYLIST SET content = ${et_content.text};"
+            database.execSQL(query)
+            Toast.makeText(this@DialogCustom, "오늘의 일기가 수정되었습니다.", Toast.LENGTH_SHORT).show()
+            finish()
         }
     }
+
+    //CustomDialog의 취소 버튼 클릭 시
     inner class CancelButtonListener: View.OnClickListener{
         override fun onClick(v: View?) {
-            Log.i("minhxxk", "취소 버튼 클릭")
+            finish()
         }
 
     }
